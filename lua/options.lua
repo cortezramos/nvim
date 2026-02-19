4a9f3b423ff33ce40b1776503d41597f413eefd2
ecortez <cortezramos@hotmail.com>
2026-02-19 11:27:39 -0600

    terminal

--- lua/options.lua
+++ lua/options.lua
@@ -11,23 +11,24 @@ vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#838383", ctermfg = 8 })
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "both"
 
+vim.opt.shell = "/opt/homebrew/bin/fish"
 
 vim.opt.laststatus = 3
 vim.opt.winbar = "%f"
 
 vim.opt.fillchars = {
-  horiz = '━',
-  horizup = '┻',
-  horizdown = '┳',
-  vert = '┃',
-  vertleft = '┫',
-  vertright = '┣',
-  verthoriz = '╋',
+  horiz = "━",
+  horizup = "┻",
+  horizdown = "┳",
+  vert = "┃",
+  vertleft = "┫",
+  vertright = "┣",
+  verthoriz = "╋",
 }
 
 -- Esto le da un color sutil a los bordes para que resalten
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#7aa2f7", bg = "none" })
 
--- Esto hace que el cursor siempre sea una barra en modo insertar, 
+-- Esto hace que el cursor siempre sea una barra en modo insertar,
 -- ayudándote a ver exactamente dónde vas a escribir.
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
