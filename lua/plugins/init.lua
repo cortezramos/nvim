0b92eaa9d936ad3bb447bce317f89e0ad69825ff
ecortez <cortezramos@hotmail.com>
2026-07-22 15:52:21 -0600

    add octo vim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -607,10 +607,29 @@ return {
     },
   },
 
-  {
-    "tpope/vim-dadbod",
-    lazy = true,
-  },
+   {
+     "pwntester/octo.nvim",
+     cmd = "Octo",
+     opts = {
+       picker = "snacks",
+       enable_builtin = true,
+     },
+     dependencies = {
+       "nvim-lua/plenary.nvim",
+       "nvim-tree/nvim-web-devicons",
+     },
+     keys = {
+       { "<leader>oi", "<CMD>Octo issue list<CR>", desc = "List GitHub Issues" },
+       { "<leader>op", "<CMD>Octo pr list<CR>", desc = "List GitHub PullRequests" },
+       { "<leader>od", "<CMD>Octo discussion list<CR>", desc = "List GitHub Discussions" },
+       { "<leader>on", "<CMD>Octo notification list<CR>", desc = "List GitHub Notifications" },
+     },
+   },
+
+   {
+     "tpope/vim-dadbod",
+     lazy = true,
+   },
   {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = { "tpope/vim-dadbod" },
